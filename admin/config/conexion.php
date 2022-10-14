61a573d5a2cd594f5bd79e4ae9c2cd63c5e6af10
ivan13gutierrez <ivancuello92@gmail.com>
2022-10-14 08:30:57 -0500

    se cambio la conexion dela bd

--- admin/config/conexion.php
+++ admin/config/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "bdsitio");
+$conn = new mysqli("bg8cek4jkgszjwusturj-mysql.services.clever-cloud.com", "uxmtkjwslezpihhv", "uxmtkjwslezpihhv", "bg8cek4jkgszjwusturj");
   
   if ($conn->connect_error) {
     die("ERROR: No se puede conectar al servidor: " . $conn->connect_error);
